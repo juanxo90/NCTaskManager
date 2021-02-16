06a499a680cf23e21cb26691583ab1f816d29551
juanxo90 <juanluiscc9@gmail.com>
2021-02-16 10:23:41 -0600

    Change

--- src/main/java/mx/edu/j2se/carrillo/tasks/Main.java
+++ src/main/java/mx/edu/j2se/carrillo/tasks/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		System.out.println(estudianteNC.getNombre() + " " + loDejaron);
 
 		Persona estudianteNC2 = new Persona(); // Crear nuevo objeto
-		estudianteNC2.setNombre("Arturo");
+		estudianteNC2.setNombre("Arturo Herrera");
 		estudianteNC2.setEdad(12);
 		loDejaron = estudianteNC2.loDejanEntrarAlBar();
 		System.out.println(estudianteNC2.getNombre() + " " + loDejaron);
